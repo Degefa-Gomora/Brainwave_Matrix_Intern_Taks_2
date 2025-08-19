b5d69f26d4af2223b1b95c2dd93dc979b59c7413
Degefa Gomora <degefag21@gmail.com>
2025-08-19 14:58:13 +0300

    role added

--- Backend/Middlewares/Authorization/auth.js
+++ Backend/Middlewares/Authorization/auth.js
@@ -1,35 +1,97 @@
-const CustomError = require("../../Helpers/error/CustomError");
-const User = require("../../Models/user")
-const jwt = require("jsonwebtoken");
-const asyncErrorWrapper =require("express-async-handler")
-const { isTokenIncluded ,getAccessTokenFromHeader} = require("../../Helpers/auth/tokenHelpers");
+// const CustomError = require("../../Helpers/error/CustomError");
+// const User = require("../../Models/user")
+// const jwt = require("jsonwebtoken");
+// const asyncErrorWrapper =require("express-async-handler")
+// const { isTokenIncluded ,getAccessTokenFromHeader} = require("../../Helpers/auth/tokenHelpers");
 
 
-const getAccessToRoute = asyncErrorWrapper(async(req,res,next) =>{
+// const getAccessToRoute = asyncErrorWrapper(async(req,res,next) =>{
 
-    const {JWT_SECRET_KEY} =process.env ;
+//     const {JWT_SECRET_KEY} =process.env ;
 
-    if(!isTokenIncluded(req)) {
+//     if(!isTokenIncluded(req)) {
 
-        return next(new CustomError("You are not authorized to access this route ", 401))
-    }
+//         return next(new CustomError("You are not authorized to access this route ", 401))
+//     }
 
-    const accessToken = getAccessTokenFromHeader(req)
+//     const accessToken = getAccessTokenFromHeader(req)
 
-    const decoded = jwt.verify(accessToken,JWT_SECRET_KEY) ;
+//     const decoded = jwt.verify(accessToken,JWT_SECRET_KEY) ;
 
-    const user = await User.findById(decoded.id)
+//     const user = await User.findById(decoded.id)
    
-    if(!user) {
-        return next(new CustomError("You are not authorized to access this route ", 401))
-    }
+//     if(!user) {
+//         return next(new CustomError("You are not authorized to access this route ", 401))
+//     }
 
-    req.user = user ; 
+//     req.user = user ; 
 
-    next()
+//     next()
 
-})
+// })
 
 
 
-module.exports ={getAccessToRoute}
+// module.exports ={getAccessToRoute}
+
+
+
+
+const CustomError = require("../../Helpers/error/CustomError");
+const User = require("../../Models/user");
+const jwt = require("jsonwebtoken");
+const asyncErrorWrapper = require("express-async-handler");
+const {
+  isTokenIncluded,
+  getAccessTokenFromHeader,
+} = require("../../Helpers/auth/tokenHelpers");
+
+// Middleware to check if the user is authenticated (logged in)
+const getAccessToRoute = asyncErrorWrapper(async (req, res, next) => {
+  const { JWT_SECRET_KEY } = process.env;
+
+  // Check if a token is included in the request headers
+  if (!isTokenIncluded(req)) {
+    return next(
+      new CustomError("You are not authorized to access this route", 401)
+    );
+  }
+
+  // Get the access token from the header
+  const accessToken = getAccessTokenFromHeader(req);
+
+  // Verify the token and decode the user's ID
+  const decoded = jwt.verify(accessToken, JWT_SECRET_KEY);
+
+  // Find the user in the database based on the decoded ID
+  const user = await User.findById(decoded.id);
+
+  // If no user is found, the token is invalid or the user no longer exists
+  if (!user) {
+    return next(
+      new CustomError("You are not authorized to access this route", 401)
+    );
+  }
+
+  // Attach the user object to the request for subsequent middleware/controllers
+  req.user = user;
+
+  next();
+});
+
+// Middleware to check if the authenticated user has the 'admin' role
+const getAdminAccess = (req, res, next) => {
+  // This middleware must be used AFTER getAccessToRoute,
+  // as it relies on req.user being populated.
+  if (req.user.role !== "admin") {
+    return next(
+      new CustomError("Only admins are authorized to access this route", 403)
+    );
+  }
+  next();
+};
+
+module.exports = {
+  getAccessToRoute,
+  getAdminAccess,
+};
--- Backend/Routers/story.js
+++ Backend/Routers/story.js
@@ -1,26 +1,97 @@
-const express = require("express")
+// const express = require("express")
+// const imageupload = require("../Helpers/Libraries/imageUpload");
+
+// const { getAccessToRoute } = require("../Middlewares/Authorization/auth");
+// const {addStory,getAllStories,detailStory,likeStory, editStory, deleteStory, editStoryPage } = require("../Controllers/story")
+// const { checkStoryExist, checkUserAndStoryExist } = require("../Middlewares/database/databaseErrorhandler");
+
+// const router = express.Router() ;
+
+// router.post("/addstory" ,[getAccessToRoute, imageupload.single("image")],addStory)
+
+
+// router.post("/:slug", checkStoryExist, detailStory)
+
+// router.post("/:slug/like",[getAccessToRoute,checkStoryExist] ,likeStory)
+
+// router.get("/editStory/:slug",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist] , editStoryPage)
+
+// router.put("/:slug/edit",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist, imageupload.single("image")] ,editStory)
+
+// router.delete("/:slug/delete",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist] ,deleteStory)
+
+// router.get("/getAllStories",getAllStories)
+
+
+// module.exports = router
+
+
+
+const express = require("express");
 const imageupload = require("../Helpers/Libraries/imageUpload");
 
-const { getAccessToRoute } = require("../Middlewares/Authorization/auth");
-const {addStory,getAllStories,detailStory,likeStory, editStory, deleteStory, editStoryPage } = require("../Controllers/story")
-const { checkStoryExist, checkUserAndStoryExist } = require("../Middlewares/database/databaseErrorhandler");
+const {
+  getAccessToRoute,
+  getAdminAccess,
+} = require("../Middlewares/Authorization/auth");
+const {
+  addStory,
+  getAllStories,
+  detailStory,
+  likeStory,
+  editStory,
+  deleteStory,
+  editStoryPage,
+} = require("../Controllers/story");
+const {
+  checkStoryExist,
+  checkUserAndStoryExist,
+} = require("../Middlewares/database/databaseErrorhandler");
 
-const router = express.Router() ;
+const router = express.Router();
 
-router.post("/addstory" ,[getAccessToRoute, imageupload.single("image")],addStory)
+// Public route - anyone can get all stories
+router.get("/getAllStories", getAllStories);
 
+// Authenticated user route - a logged-in user can add a story
+router.post(
+  "/addstory",
+  [getAccessToRoute, imageupload.single("image")],
+  addStory
+);
 
-router.post("/:slug", checkStoryExist, detailStory)
+// Public route - anyone can see a story's details
+router.post("/:slug", checkStoryExist, detailStory);
 
-router.post("/:slug/like",[getAccessToRoute,checkStoryExist] ,likeStory)
+// Authenticated user route - a logged-in user can like a story
+router.post("/:slug/like", [getAccessToRoute, checkStoryExist], likeStory);
 
-router.get("/editStory/:slug",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist] , editStoryPage)
+// Admin route - only a logged-in admin can edit a story page
+// Note: This assumes only admins should see the edit page. If not, you might need a different middleware.
+router.get(
+  "/editStory/:slug",
+  [getAccessToRoute, getAdminAccess, checkStoryExist, checkUserAndStoryExist],
+  editStoryPage
+);
 
-router.put("/:slug/edit",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist, imageupload.single("image")] ,editStory)
+// Admin route - only a logged-in admin can edit a story
+router.put(
+  "/:slug/edit",
+  [
+    getAccessToRoute,
+    getAdminAccess,
+    checkStoryExist,
+    checkUserAndStoryExist,
+    imageupload.single("image"),
+  ],
+  editStory
+);
 
-router.delete("/:slug/delete",[getAccessToRoute,checkStoryExist,checkUserAndStoryExist] ,deleteStory)
+// Admin route - only a logged-in admin can delete a story
+router.delete(
+  "/:slug/delete",
+  [getAccessToRoute, getAdminAccess, checkStoryExist, checkUserAndStoryExist],
+  deleteStory
+);
 
-router.get("/getAllStories",getAllStories)
-
-
-module.exports = router
+module.exports = router;
